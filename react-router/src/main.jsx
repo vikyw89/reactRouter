f59279c7a73b0260fed122145c1332d967bc020a
vikyw89 <vikyw89@gmail.com>
2023-02-06 15:05:32 +0800

    useLoaderData

---
+++ react-router/src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./error-page";
+import "./index.css";
+import Contact from "./routes/contact";
+import Root, { loader as rootLoader } from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    loader: rootLoader,
+    children:[
+      {
+        path: "contacts/:contactId",
+        element: <Contact/>
+      }
+    ]
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
